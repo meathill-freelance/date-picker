13153aacedc5c8e2ccdb45cef367d99ea1dc3fdc
meathill <meathill@gmail.com>
2017-06-15 18:18:32 +0800

    增加 ESLint 检查
    Signed-off-by: meathill <meathill@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loaders: ['babel-loader', 'eslint-loader']
       },
       {
         test: /\.hbs$/,
